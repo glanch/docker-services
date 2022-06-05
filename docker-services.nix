88f72431165efdcee4f493256875240e13a81755
glanch <49610590+glanch@users.noreply.github.com>
2022-06-06 00:18:17 +0200

    Undo disabled modules

--- docker-services.nix
+++ docker-services.nix
@@ -138,8 +138,8 @@ let
 
 in
 {
-  disabledModules = ["virtualisation/oci-containers.nix"];
-  imports = [ "${nixpkgsFork}/nixos/modules/virtualisation/oci-containers.nix" ];
+  # disabledModules = ["virtualisation/oci-containers.nix"];
+  # imports = [ "${nixpkgsFork}/nixos/modules/virtualisation/oci-containers.nix" ];
 
   options.virtualisation.dockerServices = {
     services = mkOption {
