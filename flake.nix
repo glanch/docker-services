1d1aa2ddaf1d1a079ae3301560eb3fda0b385bb4
glanch <49610590+glanch@users.noreply.github.com>
2022-06-06 00:02:14 +0200

    Test commit: merge

--- flake.nix
+++ flake.nix
@@ -145,7 +145,7 @@
         in
         {
           # disabledModules = [ "virtualisation/oci-containers.nix" ];
-          # imports = [ "${nixpkgsFork}/nixos/modules/virtualisation/oci-containers.nix" ];
+          imports = [ "${nixpkgsFork}/nixos/modules/virtualisation/oci-containers.nix" ];
 
           options.virtualisation.dockerServices = {
             services = mkOption {
